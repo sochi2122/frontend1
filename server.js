f41c88c437ca63147ef5393054ceba31f9cbbc49
sochi2122 <90932673+sochi2122@users.noreply.github.com>
2022-01-08 14:39:54 -0500

    lets server

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const morgan = require("morgan")
 
-///////////////////////////////
 // DATABASE CONNECTION
 ////////////////////////////////
 // Establish Connection
